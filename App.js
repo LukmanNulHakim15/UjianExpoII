1545aa96984c35f4569644e58b4361c184607321
buluk15 <lukmannul15@gmail.com>
2021-05-31 09:54:11 +0700

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import {createStackNavigator} from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native';
+import Register from './src/Page/Register';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createStackNavigator();
+ class App extends Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Register" component={Register}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ src/Page/Register.js
@@ -0,0 +1,82 @@
+import axios from 'axios';
+import React, { Component } from 'react'
+import { Text, View, StyleSheet, TextInput, onChangeText, TouchableOpacity } from 'react-native'
+
+ class Register extends Component {
+     
+    constructor(props){
+        super(props);
+        this.state={
+            username:"",
+            jenisKelamin:"",
+            phone:"",
+            umur:"",
+            gambar:""
+        }
+
+    }
+    AddRegister() {
+        axios.post('')
+        .then((response)=> {
+            alert(response.data)    
+        })
+        .catch((error)=>{
+            console.log("Eror bro, cek lagi" + error)
+        })
+    }
+
+
+    
+    render() {
+        return (
+            <View>
+               <Text> Name </Text>
+                <TextInput
+                style={styles.input}
+                onChangeText={(data)=>{this.setState({username:data})}}
+                placeholder="Username"
+                />
+
+                <Text> Jenis Kelamin </Text>
+                <TextInput
+                style={styles.input}
+                onChangeText={(data)=>{this.setState({jenisKelamin:data})}}
+                placeholder="JenisKelamin"
+                />
+
+                <Text> Phone </Text>
+                <TextInput
+                style={styles.input}
+                onChangeText={(data)=>{this.setState({phone:data})}}
+                placeholder="Phone"
+                />
+
+                <Text> umur </Text>
+                <TextInput
+                style={styles.input}
+                onChangeText={(data)=>{this.setState({umur:data})}}
+                placeholder="Umur"
+                />
+
+                <TouchableOpacity style={styles.button} onPress={this.AddRegister.bind(this)}><Text style={styles.title}>Registrasi</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={()=>{this.props.navigation.navigate("Login")}}><Text style={styles.title}>Home</Text></TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+export default Register;
+
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+      },
+
+      button: {
+        alignItems: "center",
+        backgroundColor: "#DDDDDD",
+        padding: 10,
+      },
+});
